11c0e03eb98af5cfe42bb2d0ec856ca21a22cd0a
Rama-abwdan <ramaabwdan06@gmail.com>
2026-07-24 00:05:51 +0300

    create model Parente

--- app/Models/Parente.php
+++ app/Models/Parente.php
@@ -7,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class Parente extends Model
 {
     //
+
+    protected $table = 'parents';
+
+    protected $fillable = [
+        'full_name_father',
+        'job_father',
+        'phone_number_father',
+        'full_name_mather',
+        'job_mather',
+        'phone_number_mather',
+        'user_id',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function students()
+    {
+        return $this->hasMany(Student::class, 'parent_id');
+    }
 }
