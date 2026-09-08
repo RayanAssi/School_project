3e712f137f22b047aaa720f6e14b57ed99879705
Fatima-Kattan <fatimaqattan959@gmail.com>
2026-09-08 19:17:14 +0300

    merge controllers

--- routes/api.php
+++ routes/api.php
@@ -34,14 +34,10 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.', 'middleware' => ['a
     Route::get('teachers/{id}/subjects', [TeacherController::class, 'getSubjects']);
     Route::get('teachers/{id}/classes', [TeacherController::class, 'getClasses']);
     Route::get('teacher/my-sections', [TeacherController::class, 'getMySections']);
-<<<<<<< HEAD
     Route::put('/teachers/{id}/assign-sections-subjects', [TeacherController::class, 'assignSectionsAndSubjects']);
     Route::put('/teachers/{id}/assign-sections-subjects', [TeacherController::class, 'assignSectionsAndSubjects']);
     Route::get('/teachers/{id}/details', [TeacherController::class, 'getTeacherDetails']);
     Route::get('/teachers/{id}/edit', [TeacherController::class, 'edit']);
-=======
-
->>>>>>> e9982786be27f2d9eada56bdd91a94cfbdbab895
 
     //parents Routes
     Route::get('parents/statistics', [ParentController::class, 'statistics'])->name('parents.statistics');
