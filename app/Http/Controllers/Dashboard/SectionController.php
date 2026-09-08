1e180cec4da81fa249984bc50d52e7e7ebe06617
RayaNaSSi <rassi7266@gmail.com>
2026-09-08 15:04:01 +0300

    edit delete function in section controller

--- app/Http/Controllers/Dashboard/SectionController.php
+++ app/Http/Controllers/Dashboard/SectionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\Section;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class SectionController extends Controller
